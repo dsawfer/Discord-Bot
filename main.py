2f0957e84a9f8456faadb6d0dfddc052967a789e
Viktor Dorokhov <waveapologiesng@gmail.com>
2021-08-06 19:45:49 +0300

    Alpha 0.1.0

--- main.py
+++ main.py
@@ -1,37 +1,41 @@
 import os
+import traceback
 import discord
 from discord.ext import commands
 import requests
 import vk_api
 from vk_api.audio import VkAudio
 
+VERSION = 'Early access, alpha 0.1.0'
+
 bot = commands.Bot(command_prefix='.', description='Relatively simple music bot')
 TOKEN = os.getenv('TOKEN')
 
 REQUEST_STATUS_CODE = 200
-name_dir = 'music_vk'
-path = r'G:\Projects\BotPy\source\\' + name_dir
-# login = LOGIN  # Номер телефона
-# password = PASSWORD  # Пароль
-my_id = 'dsawfer'  # Ваш id vk
+# name_dir = 'music_vk'
+# name_song = 'song.mp3'
+# path = r'G:\Projects\BotPy\source\\' + name_dir
+LOGIN = os.getenv('LOGIN')  # vk login
+PASSWORD = os.getenv('PASSWORD')  # vk password
+VK_TOKEN = os.getenv('VK_TOKEN')
+# my_id = 'my_id'  # vk id
 
 
 class Music(commands.Cog):
 
     def __init__(self, bot):
         self.bot = bot
-        self.bot_join = False
+        # self.bot_join = False
 
     @commands.command()
     async def join(self, ctx):
         """join to current client voice channel"""
-        if self.bot_join:
+        if ctx.voice_client:
             return True
         if ctx.author.voice:
             channel = ctx.message.author.voice.channel
             print(f'{bot.user.name} join to {channel}')
             await channel.connect()
-            self.bot_join = True
             return True
         else:
             print('Voice channel is not defined')
@@ -44,7 +48,6 @@ class Music(commands.Cog):
             print(f'{bot.user.name} leave from {ctx.author.voice.channel}')
             await self.stop(ctx)  # stop playing song
             await ctx.voice_client.disconnect()  # disconnect from current voice channel
-            self.bot_join = False
             return True
         else:
             print('Voice channel is not defined')
@@ -54,26 +57,29 @@ class Music(commands.Cog):
     async def play(self, ctx,  *, query):
         if not await self.join(ctx):
             return
-        os.chdir(path)
+        # os.chdir(path)
         try:
             song = vk_audio.search(query, 1, 0)  # search query in global vk audio database
-            ref = requests.get(song.__next__().get('url'))  # get url of audio
-            if ref.status_code == REQUEST_STATUS_CODE:
-                with open('song' + '.mp3', 'wb') as output_file:
-                    output_file.write(ref.content)
-            print(ref)
+
+            # ref = requests.get(song.__next__().get('url'))  # get url of audio
+            # if ref.status_code == REQUEST_STATUS_CODE:
+            #     with open(name_song, 'wb') as output_file:
+            #         output_file.write(ref.content)
+            # print(ref)
+            ctx.voice_client.play(discord.FFmpegPCMAudio(song.__next__().get('url')))
+            await ctx.send(f'Now playing: {query}')
         except StopIteration:
-            await ctx.send('Something goes wrong, try again (StopIterator)')
+            print(traceback.format_exc())
+            await ctx.send('Cannot find the song, please try again')
         except OSError:
+            print(traceback.format_exc())
             await ctx.send('Something goes wrong, try again (OSError)')
+        except discord.errors.ClientException:
+            print(traceback.format_exc())
+            await ctx.send('Song already playing, type ".stop" and try again')
         # song_there = os.path.isfile('song.mp3')
-
-        ctx.voice_client.play(discord.FFmpegPCMAudio(path + os.sep + 'song.mp3'))
-
         # source = discord.PCMVolumeTransformer(discord.FFmpegPCMAudio(path + os.sep + 'song.mp3'))
         # ctx.voice_client.play(source, after=lambda e: print('Player error: %s' % e) if e else self.leave(ctx))
-
-        await ctx.send('Now playing: {}'.format(query))
 
     @commands.command()
     async def pause(self, ctx):
@@ -107,20 +113,23 @@ async def on_message(message):
     await bot.process_commands(message)
 
 
+@bot.command()
+async def version(ctx):
+    await ctx.send(f'Version: {VERSION}')
+
+
 @bot.command()  # разрешаем передавать агрументы (pass_context=True)
 async def test(ctx,  *, message: str):  # оздаем асинхронную фунцию бота
     await ctx.send(message)  # отправляем обратно аргумент
 
 
-if not os.path.exists(path):  # create directory if it not exists
-    os.makedirs(path)
+# if not os.path.exists(path):  # create directory if it not exists
+#     os.makedirs(path)
 
-vk_session = vk_api.VkApi('+7----------', 'mypassword')
+vk_session = vk_api.VkApi(LOGIN, VK_TOKEN)
 vk_session.auth()
 vk_audio = VkAudio(vk_session)
 # vk = vk_session.get_api()
-
-# vk_audio = audio.VkAudio(vk_session)
 
 bot.add_cog(Music(bot))
 bot.run(TOKEN)
